07a52ba7b3fd2602525af77dc69bd60deab6263c
Brian Yandell <byandell@wisc.edu>
2017-09-06 20:36:11 -0500

    moved setup_util to DOread package

--- inst/doqtl2/setup.R
+++ inst/doqtl2/setup.R
@@ -4,10 +4,10 @@ datapath <- file.path(dirpath, "DerivedData")
 pmap <- readRDS(file.path(datapath, "pmap.rds"))
 covar <- readRDS(file.path(datapath, "covar.rds"))
 
-peaks <- setup_peaks(datapath) 
-analyses_tbl <- setup_analyses(peaks, datapath)
-pheno_data <- setup_data(analyses_tbl, peaks, datapath)
-pheno_type <- setup_type(analyses_tbl)
+peaks        <- DOread::setup_peaks(datapath) 
+analyses_tbl <- DOread::setup_analyses(peaks, datapath)
+pheno_data   <- DOread::setup_data(analyses_tbl, peaks, datapath)
+pheno_type   <- DOread::setup_type(analyses_tbl)
 
 K <- readRDS(file.path(datapath, "kinship.rds"))
 
