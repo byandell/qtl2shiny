39f0d60308edc613c2d50162a342e217ebc66999
Brian Yandell <byandell@wisc.edu>
2018-04-13 14:16:27 -0500

    add intermediate to versions

--- R/versions.R
+++ R/versions.R
@@ -1,8 +1,9 @@
 versions <- function(packages = c(qtl2 = "qtl2",
                                   shiny = "qtl2shiny",
                                   pattern = "qtl2pattern",
-                                  gggplot = "qtl2ggplot", fst = "qtl2fst",
-                                  causal = "CausalMST")) {
+                                  ggplot = "qtl2ggplot", fst = "qtl2fst",
+                                  causal = "CausalMST",
+                                  mediate = "intermediate")) {
   pv <- unlist(apply(as.matrix(packages), 1, function(x) as.character(packageVersion(x))))
   paste("Version", paste(names(packages), pv, sep = "=", collapse = ", "))
 }
