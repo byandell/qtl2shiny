02f2cda10127002145d7f8a64d3f501c5f6e7680
Brian Yandell <byandell@wisc.edu>
2021-09-28 14:38:19 -0500

    remove expr_region

--- R/expr_region.R
+++
@@ -1,9 +0,0 @@
-expr_region <- function(chr_id, scan_window, covar, qtls, pmap,
-                        project_info) {
-  # Get expression mRMNA measurements.
-  query_mrna <- read_query_rds(project_info, "query_mrna.rds")
-  
-  qtl2mediate::expr_region(chr_id, scan_window[1], scan_window[2], covar, pmap, 
-                           drivers = qtls,
-                           query_mrna = query_mrna)
-}
